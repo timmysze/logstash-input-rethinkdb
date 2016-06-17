f66d08531f6141238608f6c765bb565f0ffc073e
Tim Sze <tim@compgun.com>
2016-06-17 02:23:21 -0700

    Added user and password to options

--- lib/logstash/inputs/rethinkdb.rb
+++ lib/logstash/inputs/rethinkdb.rb
@@ -32,6 +32,9 @@ class LogStash::Inputs::RethinkDB < LogStash::Inputs::Base
   config :backfill, :default => true
   #ssl support
   config :ca_certs, :default => nil
+  # Credentials as of RethinkDB v2.3.x
+  config :user, :validate => :string, :default => "admin"
+  config :password, :validate => :string, :default => ""
 
 
   # Part of the logstash input interface
